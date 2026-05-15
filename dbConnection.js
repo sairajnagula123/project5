1ec0b625aae27dcfe42d606de222f12e5503341e
Sairaj <nagulasairaj16@gmail.com>
2026-05-15 10:51:21 +0530

    updated project

--- dbConnection.js
+++ dbConnection.js
@@ -1,6 +1,6 @@
 // dbConnection.js
 
-import mongoose from "mongoose";
+import {connect} from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,7 +9,7 @@ const dbUrl = process.env.DB_URL;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbUrl);
+        await connect(dbUrl);
 
         console.log("MongoDB Connected...");
     } 
