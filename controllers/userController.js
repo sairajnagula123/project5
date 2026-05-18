e7b69cd067b7b0bd0575922cceecade0855cc423
Sairaj <nagulasairaj16@gmail.com>
2026-05-18 10:25:03 +0530

    updated project

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,27 +2,56 @@
 
 import User from "../models/User.js";
 
-export const register = async(req,res) => {
-    const {name , age, gender, email, password, city, mobile} = req.body;
+export const registerUser = async (req, res) => {
 
-    let userRecord = new UserModel({
-        name,
-        age,
-        gender,
-        email,
-        password,
-        city,
-        mobile,
-    })
+    try {
 
-    let resp = await userRecord.save();
+        const {
+            name,
+            age,
+            gender,
+            email,
+            password,
+            city,
+            mobile
+        } = req.body;
 
-    res.json({
-        message: "Success",
-        resp,
-    });
-}
+        // check existing user
+        const existingUser = await User.findOne({ email });
 
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        // create user
+        const userRecord = new User({
+            name,
+            age,
+            gender,
+            email,
+            password,
+            city,
+            mobile,
+        });
+
+        const resp = await userRecord.save();
+
+        res.status(201).json({
+            message: "User Registered Successfully",
+            data: resp,
+        });
+
+    }
+    catch (err) {
+
+        res.status(500).json({
+            message: "Registration Failed",
+            error: err.message
+        });
+    }
+};
 
 
 
@@ -32,6 +61,7 @@ export const loginUser = async (req, res) => {
 
         const { email, password } = req.body;
 
+        // check user
         const user = await User.findOne({ email });
 
         if (!user) {
@@ -40,6 +70,7 @@ export const loginUser = async (req, res) => {
             });
         }
 
+        // check password
         if (user.password !== password) {
             return res.status(401).json({
                 message: "Invalid Password"
@@ -47,13 +78,16 @@ export const loginUser = async (req, res) => {
         }
 
         res.status(200).json({
-            message: "Login Successful"
+            message: "Login Successful",
+            user
         });
 
-    } 
+    }
     catch (err) {
+
         res.status(500).json({
-            message: "Error during login"
+            message: "Error during login",
+            error: err.message
         });
     }
 };
